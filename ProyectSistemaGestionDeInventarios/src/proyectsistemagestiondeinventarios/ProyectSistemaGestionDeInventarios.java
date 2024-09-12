511d7e60ae7e436aa5c2097419ac65a3971ebbac
jnavichoc <jacquelinnavi120@gmail.com>
2024-09-11 21:32:42 -0600

    Se agrega Proyecto

---
+++ ProyectSistemaGestionDeInventarios/src/proyectsistemagestiondeinventarios/ProyectSistemaGestionDeInventarios.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectsistemagestiondeinventarios;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jacqu
+ */
+public class ProyectSistemaGestionDeInventarios {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner (System.in);
+         int opcion= 0;
+         
+        //Creacion de Menu principal 
+        System.out.println("SISTEMA DE GESTION DE INVENTARIOS\n");
+        System.out.println("Ingrese Contraseña (Administrador):");
+        opcion = entrada.nextInt();
+    }
+    
+}
